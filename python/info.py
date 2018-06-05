654db1cd87b03f2ce76925aeaa2796190e9a89b2
nearhope <near@hope.edu>
2018-06-05 11:47:22 -0400

    added print functions

--- python/info.py
+++ python/info.py
@@ -1,2 +1,7 @@
 
 myInfo={"firstName":"andy", "lastName":"near", "hometown":"holland", "favFood":"dal"}
+myInfo["favoriteInstructor"]="tony"
+myInfo["goodFood"]="spinach"
+print(myInfo["firstName"] + ", " + myInfo["lastName"])
+print(myInfo["hometown"])
+print(myInfo["favFood"] + " and " + myInfo["goodFood"])
