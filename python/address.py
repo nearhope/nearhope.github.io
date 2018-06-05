71a500e2d55b6e050a15fb218a75cc76e7f83233
nearhope <near@hope.edu>
2018-06-05 14:54:20 -0400

    changes

---
+++ python/address.py
@@ -0,0 +1,18 @@
+
+first="andy"
+last="near"
+street="123 1ST ST"
+city="Philadelphia Center"
+state="Illinois"
+zip="12345-6789"
+
+print(first + " " + last)
+print(street)
+print(city + ", " + state + " " + zip)
+print()
+
+address={"first":"andy","last":"near","street":"123 1st St","city":"Philadelphia Center","state":"Illinois","zip":"12345-6789"}
+
+print(address["first"] + " " + address["last"])
+print(address["street"])
+print(address["city"] + ", " + address["state"] + " " + address["zip"])
